03e524673cb0fa790ef0ebed416edb5be2e27ffd
Tom White <eugene.white@gmail.com>
2019-08-24 04:26:49 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,10 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
+ 
+  def down
+  end
+  
   def change
     create_table :artists do |t|
       t.string :name
